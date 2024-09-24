584ffd41e1dd8b3b63487dec9ca356ddb1299279
Nira Jain <s223144439@deakin.edu.au>
2024-09-25 01:43:22 +1000

    Initial commit

---
+++ Jenkins/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Jenkins;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        // Custom endpoint for welcome message
+        app.MapGet("/", () => "Hi, welcome to Jenkins!");
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
+    }
+}
